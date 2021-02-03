7d736ea43a86321959d5576db2d38e5e2ac976eb
dusty2035 <67262688+dusty2035@users.noreply.github.com>
2021-02-03 12:55:06 +0330

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import NewSerilizer
-from rest_framework import permissions
 from .models import New
 # Create your views here.
 
