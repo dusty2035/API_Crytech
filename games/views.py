ccbb561ea0594e6a4fdfe43de05bf8b53e611417
dusty2035 <67262688+dusty2035@users.noreply.github.com>
2021-02-03 12:53:57 +0330

    Update views.py

--- games/views.py
+++ games/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import GameSerilizer
-from rest_framework import permissions
 from .models import Game
 # Create your views here.
 
